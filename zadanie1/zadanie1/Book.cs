ec27e1f180521af05341125968cb305f1ff0731f
Trancever <dawidu@onet.pl>
2017-02-28 22:35:36 -0800

    Added first project template

---
+++ zadanie1/zadanie1/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class Book
+    {
+        public Book(string name, string author)
+        {
+            Name = name;
+            Author = author;
+        }
+
+        public string description()
+        {
+            return "Book name is " + Name + ". It's author name is " + Author + "\n";
+        }
+
+        private string Name { get; }
+        private string Author { get; }
+    }
+}
---
+++ zadanie1/zadanie1/Reader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class Reader
+    {
+        public Reader(string firstName, string lastName)
+        {
+            Firstname = firstName;
+            LastName = lastName;
+        }
+
+        private string Firstname { get; }
+        private string LastName { get; }
+    }
+}
